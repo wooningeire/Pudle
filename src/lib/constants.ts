adb0af71a826861925768e96414fc3450fb69e76
wooningeire <22846982+wooningeire@users.noreply.github.com>
2025-05-16 05:26:35 -0400

    feat: tweak constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,11 +3,11 @@ export const ISLAND_SIZE_THRESHOLD = 4;
 export const WORD_LENGTH = 5;
 
 export const MAX_TIME_LIMIT_S_BY_WORD_NO = 45;
-export const MIN_TIME_LIMIT_S_BY_WORD_NO = 5;
-export const MAX_TIME_DECAY_LIMIT_S_BY_GUESS_NO = 15;
-export const MIN_TIME_DECAY_LIMIT_S_BY_GUESS_NO = 4;
-export const GUESS_TIME_BY_WORD_NO_DECAY_FAC = 1/10.5;
-export const GUESS_TIME_BY_GUESS_NO_DECAY_FAC = 1/6;
+export const MIN_TIME_LIMIT_S_BY_WORD_NO = 3.5;
+export const MAX_TIME_DECAY_LIMIT_S_BY_GUESS_NO = 12;
+export const MIN_TIME_DECAY_LIMIT_S_BY_GUESS_NO = 2.5;
+export const GUESS_TIME_BY_WORD_NO_DECAY_FAC = 1/12;
+export const GUESS_TIME_BY_GUESS_NO_DECAY_FAC = 1/8;
 export const PAR_GUESSES_PER_WORD = 4;
 
 export const EMPTY_TILE_CHAR = " ";
